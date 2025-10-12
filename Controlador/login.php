547b27f37ff416d9a8d51783b0d6f75bec62e51d
Marcosoz <marcosozaro@vera.com.uy>
2025-10-12 14:29:34 -0300

    Se crea pagina de login con conexion a db. Queda funcionando

---
+++ Controlador/login.php
@@ -0,0 +1,16 @@
+<?php
+
+if (!empty($_POST["btn_ingresar"])) {
+    if (empty($_POST["usuario"]) and empty($_POST["password"])) {
+        echo '<div class="alert alert-danger">Los campos estan vacios</div>';
+    } else {
+        $usuario = $_POST["usuario"];
+        $clave = $_POST["password"];
+        $sql = $conexion->query(" SELECT nombre_completo, contraseña FROM socios WHERE nombre_completo='$usuario' and contraseña='$clave' ");
+        if ($datos = $sql->fetch_object()) {
+            header("location:index.php");
+        } else {
+            echo '<div class="alert alert-danger">Acceso Denegado</div>';
+        }
+    }
+}
