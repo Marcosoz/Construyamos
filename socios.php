a3677a3014d59dc6015149d50795acb99790d0ee
Marcosoz <marcosozaro@vera.com.uy>
2025-10-14 20:59:26 -0300

    cambios en vista de socios, agregar y modificar socios

--- socios.php
+++ socios.php
@@ -5,76 +5,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Socios</title>
-    <!-- CSS ONLY -->
+    <!-- CSS ONLY class="col-8 p-4" -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/ade5209d09.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/tablestyle.css">
 </head>
 
 <body>
     <h1 class="text-center p-3">Socios</h1>
     <div class="container-flex">
         <a class="btn btn-primary" href="home.php" role="button">Inicio</a>
-    </div>
+
+        <a class="btn btn-secondary" href="agregarSocio.php" role="button">Agregar Socio</a>
+
     <div class="container-fluid row">
-        <form class="col-4 p-3" method="POST">
-            <h3 class="text-center alert alert-secondary">Ingreso de socios</h3>
-            <?php
-            include "Modelo/conexion.php";
-            include "Controlador/registro_socios.php";
-            ?>
-            <!--<div class="mb-3">
-                    <label for="disabledTextInput" class="form-label" hidden>CooperativaID</label>
-                    <input type="hidden" id="disabledTextInput" class="form-control" name="cooperativaID">
-                </div>-->
-            <div class="mb-3">
-                <label for="disabledTextInput" class="form-label">Nombre completo</label>
-                <input type="text" id="disabledTextInput" class="form-control" name="nombre">
-            </div>
-            <div class="mb-3">
-                <label for="disabledTextInput" class="form-label">Documento</label>
-                <input type="text" id="disabledTextInput" class="form-control" name="documento">
-            </div>
-            <div class="mb-3">
-                <label for="disabledTextInput" class="form-label">Telefono</label>
-                <input type="text" id="disabledTextInput" class="form-control" name="telefono">
-            </div>
-            <div class="mb-3">
-                <label for="disabledTextInput" class="form-label">Email</label>
-                <input type="text" id="disabledTextInput" class="form-control" name="email">
-            </div>
-            <div class="mb-3">
-                <label for="disabledTextInput" class="form-label">Clave de acceso</label>
-                <input type="text" id="disabledTextInput" class="form-control" name="clave">
-            </div>
-            <div class="mb-3">
-                <label for="disabledTextInput" class="form-label">Fecha de ingreso</label>
-                <input type="date" id="disabledTextInput" class="form-control" name="fecha_ingreso">
-            </div>
-            <!--                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" value="" id="checkDefault" name="socio_si[]">
-                    <label class="form-check-label" for="checkDefault">
-                        Socio Si
-                    </label>
-                </div> -->
-            <div class="mb-3">
-                <label for="disabledSelect" class="form-label">Nivel de usuario</label>
-                <select id="disabledSelect" class="form-select" name="nivel_usuario">
-                    <option value="1">Administrador</option>
-                    <option value="2">socio</option>
-                </select>
-            </div>
-            <div class="mb-3">
-                <label for="disabledSelect" class="form-label">Cupo</label>
-                <select id="disabledSelect" class="form-select" name="cupo">
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                </select>
-            </div>
-            <button type="submit" class="btn btn-primary" name="btn_registro" value="ok">Registrar</button>
-        </form>
-        <div class="col-8 p-4">
-            <table class="table">
+        <div>
+            <table class="table_2">
                 <thead class="bg-info">
                     <tr>
                         <th scope="col">Nombre</th>
@@ -109,10 +55,9 @@
                 </tbody>
             </table>
         </div>
-
     </div>
-
-
+    
+    </div>
 
     <!-- JavaScript Bundle with popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
